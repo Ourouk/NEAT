c480c74a75d7a9259c0bdbacb20e48c42ca42b7f
Andrea Spelgatti <spelgattiandrea@gmail.com>
2024-12-29 12:41:06 +0100

    :tada: Initial Connection Class

---
+++ src/main/java/com/hepl/NEAT/Connection.java
@@ -0,0 +1,42 @@
+package com.hepl.NEAT;
+
+public class Connection {
+    public enum State {
+        ENABLED,DISABLED
+    }
+    // Store information about connected nodes
+    // These are finals after the creation
+    private final Node inputNode;
+    private final Node outputNode;
+    // The connection state say if the connection is used
+    // Can change during the training
+    private State connectionState;
+    // The multiplier used when using this connection
+    private Integer weight;
+    Connection(Node Input, Node Output,Integer weight) {
+        this.inputNode = Input;
+        this.outputNode = Output;
+        this.connectionState = State.ENABLED;
+        this.weight = weight;
+    }
+
+    public Node getInputNode() {
+        return inputNode;
+    }
+    public Node getOutputNode() {
+        return outputNode;
+    }
+    public State getConnectionState() {
+        return connectionState;
+    }
+    public void setConnectionState(State connectionState) {
+        this.connectionState = connectionState;
+    }
+    public Integer getWeight() {
+        return weight;
+    }
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+}
