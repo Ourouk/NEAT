e242ff8518b0f6a1293a7df50b031e3351ba30da
Martin <martin.vandenbossche@yahoo.com>
2025-01-05 23:16:31 +0100

    TestSpeciation correct but bugged

--- src/main/java/com/hepl/test/TestSpeciation.java
+++ src/main/java/com/hepl/test/TestSpeciation.java
@@ -7,90 +7,124 @@ import java.util.List;
 public class TestSpeciation {
 
     public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        // Create nodes and connections
-        Node node1 = new Node(Node.Type.INPUT);
-        Node node2 = new Node(Node.Type.INPUT);
-        Node node3 = new Node(Node.Type.INPUT);
-        Node node4 = new Node(Node.Type.OUTPUT);
-        Node node5 = new Node(Node.Type.HIDDEN);
-        Node node6 = new Node(Node.Type.HIDDEN);
-        
-        Connection con1 = new Connection(node1, node4, 1);
-		Connection con2 = new Connection(node2, node4, 1);
-		Connection con3 = new Connection(node3, node4, 1);
-		Connection con4 = new Connection(node2, node5, 1);
-		Connection con5 = new Connection(node5, node4, 1);
-		Connection con6 = new Connection(node5, node6, 1);
-		Connection con7 = new Connection(node6, node4, 1);
-		Connection con8 = new Connection(node1, node5, 1);
-		Connection con9 = new Connection(node3, node5, 1);
-		Connection con10 = new Connection(node1, node6, 1);
-        
-        // Create genomes with varying structures (Two are same as the TestMatchingDisjointExcess)
+        // Parent 1
         Genome parent1 = new Genome();
-        
-        parent1.addNode(node1);
-        parent1.addNode(node2);
-        parent1.addNode(node3);
-        parent1.addNode(node4);
-        parent1.addNode(node5);
-        
-		con2.setConnectionState(Connection.State.DISABLED);
-		
-        parent1.addConnection(con1);
-        parent1.addConnection(con2);
-        parent1.addConnection(con3);
-        parent1.addConnection(con4);
-        parent1.addConnection(con5);
-        parent1.addConnection(con8);
-        
-        
-        
-        
+
+        Node p1Node1I = new Node(Node.Type.INPUT);
+        Node p1Node2I = new Node(Node.Type.INPUT);
+        Node p1Node3I = new Node(Node.Type.INPUT);
+        Node p1Node4O = new Node(Node.Type.OUTPUT);
+        Node p1Node5H = new Node(Node.Type.HIDDEN);
+
+        parent1.addNode(p1Node1I);
+        parent1.addNode(p1Node2I);
+        parent1.addNode(p1Node3I);
+        parent1.addNode(p1Node4O);
+        parent1.addNode(p1Node5H);
+
+        Connection p1Con14 = new Connection(p1Node1I, p1Node4O, 1);
+        Connection p1Con24 = new Connection(p1Node2I, p1Node4O, 1);
+        Connection p1Con34 = new Connection(p1Node3I, p1Node4O, 1);
+        Connection p1Con25 = new Connection(p1Node2I, p1Node5H, 1);
+        Connection p1Con54 = new Connection(p1Node5H, p1Node4O, 1);
+        Connection p1Con15 = new Connection(p1Node1I, p1Node5H, 1);
+
+        p1Con24.setConnectionState(Connection.State.DISABLED);
+
+        parent1.addConnection(p1Con14);
+        parent1.addConnection(p1Con24);
+        parent1.addConnection(p1Con34);
+        parent1.addConnection(p1Con25);
+        parent1.addConnection(p1Con54);
+        parent1.addConnection(p1Con15);
+
+        parent1.exportToDot("Images/Speciation/parent1.dot");
+
+        // Parent 2
         Genome parent2 = new Genome();
-        
-        parent2.addNode(node1.clone());
-        parent2.addNode(node2.clone());
-        parent2.addNode(node3.clone());
-        parent2.addNode(node4.clone());
-        parent2.addNode(node5.clone());
-        parent2.addNode(node6.clone());
 
-        Connection newCon5 = con5.clone();
-        newCon5.setConnectionState(Connection.State.DISABLED);
-        
-        parent2.addConnection(con1.clone());
-        parent2.addConnection(con2.clone());
-        parent2.addConnection(con3.clone());
-        parent2.addConnection(con4.clone());
-        parent2.addConnection(newCon5);
-        parent2.addConnection(con6.clone());
-        parent2.addConnection(con7.clone());
-        parent2.addConnection(con9.clone());
-        parent2.addConnection(con10.clone());
-        
-        
-        
-        
+        Node p2Node1I = new Node(Node.Type.INPUT);
+        Node p2Node2I = new Node(Node.Type.INPUT);
+        Node p2Node3I = new Node(Node.Type.INPUT);
+        Node p2Node4O = new Node(Node.Type.OUTPUT);
+        Node p2Node5H = new Node(Node.Type.HIDDEN);
+        Node p2Node6H = new Node(Node.Type.HIDDEN);
+
+        parent2.addNode(p2Node1I);
+        parent2.addNode(p2Node2I);
+        parent2.addNode(p2Node3I);
+        parent2.addNode(p2Node4O);
+        parent2.addNode(p2Node5H);
+        parent2.addNode(p2Node6H);
+
+        Connection p2Con14 = new Connection(p2Node1I, p2Node4O, 1);
+        Connection p2Con24 = new Connection(p2Node2I, p2Node4O, 1);
+        Connection p2Con34 = new Connection(p2Node3I, p2Node4O, 1);
+        Connection p2Con25 = new Connection(p2Node2I, p2Node5H, 1);
+        Connection p2Con54 = new Connection(p2Node5H, p2Node4O, 1);
+        Connection p2Con56 = new Connection(p2Node5H, p2Node6H, 1);
+        Connection p2Con64 = new Connection(p2Node6H, p2Node4O, 1);
+        Connection p2Con35 = new Connection(p2Node3I, p2Node5H, 1);
+        Connection p2Con16 = new Connection(p2Node1I, p2Node6H, 1);
+
+        p2Con24.setConnectionState(Connection.State.DISABLED);
+        p2Con54.setConnectionState(Connection.State.DISABLED);
+
+        parent2.addConnection(p2Con14);
+        parent2.addConnection(p2Con24);
+        parent2.addConnection(p2Con34);
+        parent2.addConnection(p2Con25);
+        parent2.addConnection(p2Con54);
+        parent2.addConnection(p2Con56);
+        parent2.addConnection(p2Con64);
+        parent2.addConnection(p2Con35);
+        parent2.addConnection(p2Con16);
+
+        parent2.exportToDot("Images/Speciation/parent2.dot");
+
+        // Offspring
         Genome offspring = new Genome();
-        offspring.addNode(node1.clone());
-        offspring.addNode(node2.clone());
-        offspring.addNode(node3.clone());
-        offspring.addNode(node4.clone());
-        offspring.addNode(node5.clone());
-        offspring.addNode(node6.clone());
 
-        offspring.addConnection(con1.clone());
-        offspring.addConnection(con2.clone());
-        offspring.addConnection(con3.clone());
-        offspring.addConnection(con4.clone());
-        offspring.addConnection(newCon5.clone());
-        offspring.addConnection(con6.clone());
-        offspring.addConnection(con7.clone());
-        offspring.addConnection(con8.clone());
-        offspring.addConnection(con9.clone());
-        offspring.addConnection(con10.clone());
+        Node oNode1I = new Node(Node.Type.INPUT);
+        Node oNode2I = new Node(Node.Type.INPUT);
+        Node oNode3I = new Node(Node.Type.INPUT);
+        Node oNode4O = new Node(Node.Type.OUTPUT);
+        Node oNode5H = new Node(Node.Type.HIDDEN);
+        Node oNode6H = new Node(Node.Type.HIDDEN);
+
+        offspring.addNode(oNode1I);
+        offspring.addNode(oNode2I);
+        offspring.addNode(oNode3I);
+        offspring.addNode(oNode4O);
+        offspring.addNode(oNode5H);
+        offspring.addNode(oNode6H);
+
+        Connection oCon14 = new Connection(oNode1I, oNode4O, 1);
+        Connection oCon24 = new Connection(oNode2I, oNode4O, 1);
+        Connection oCon34 = new Connection(oNode3I, oNode4O, 1);
+        Connection oCon25 = new Connection(oNode2I, oNode5H, 1);
+        Connection oCon54 = new Connection(oNode5H, oNode4O, 1);
+        Connection oCon56 = new Connection(oNode5H, oNode6H, 1);
+        Connection oCon64 = new Connection(oNode6H, oNode4O, 1);
+        Connection oCon15 = new Connection(oNode1I, oNode5H, 1);
+        Connection oCon35 = new Connection(oNode3I, oNode5H, 1);
+        Connection oCon16 = new Connection(oNode1I, oNode6H, 1);
+
+        oCon24.setConnectionState(Connection.State.DISABLED);
+        oCon54.setConnectionState(Connection.State.DISABLED);
+
+        offspring.addConnection(oCon14);
+        offspring.addConnection(oCon24);
+        offspring.addConnection(oCon34);
+        offspring.addConnection(oCon25);
+        offspring.addConnection(oCon54);
+        offspring.addConnection(oCon56);
+        offspring.addConnection(oCon64);
+        offspring.addConnection(oCon15);
+        offspring.addConnection(oCon35);
+        offspring.addConnection(oCon16);
+
+        offspring.exportToDot("Images/Speciation/offspring.dot");
 
         // Englobe genomes with fitness
         GenomeWithFitness gwf1 = new GenomeWithFitness(parent1, 1.0f);
@@ -141,7 +175,7 @@ public class TestSpeciation {
             // show genomes in the species
             System.out.println("  Contains the following genomes:");
             for (GenomeWithFitness genome : species.getGenomes()) {
-                System.out.println("    Genome Fitness: " + genome.getFitness() + ", Connections: " + genome.getGenome().connections.size());
+                System.out.println("    Connections: " + genome.getGenome().connections.size());
             }
             speciesIndex++;
         }
