bf72ef173259a0ffd7fe63165e9e64dbe777a555
Andrea Spelgatti <spelgattiandrea@gmail.com>
2025-01-05 15:35:59 +0100

    Improving sigmoid + better handling of bias and no hiddenlayer scenario

--- src/main/java/com/hepl/NEAT/Genome.java
+++ src/main/java/com/hepl/NEAT/Genome.java
@@ -39,11 +39,19 @@ public class Genome {
         connections.add(c);
     }
     //Automatic connection creation
-    public void addConnection(com.hepl.NEAT.Node Input, com.hepl.NEAT.Node Output, Float weight){
+    public boolean addConnection(com.hepl.NEAT.Node Input, com.hepl.NEAT.Node Output, Float weight){
+        //Check if the connection already exist
+        for(Connection c : connections){
+            if(c.getInputNode() == Input && c.getOutputNode() == Output){
+                return false;
+            }
+        }
+        //Create a connection
         Connection c =new Connection(Input,Output,weight);
         connections.add(c);
         Input.addOutgoingConnection(c);
         Output.addIncomingConnection(c);
+        return true;
     }
 
     //Access an object into the treeMap
@@ -53,44 +61,55 @@ public class Genome {
 
     //Generate and evaluate the output of the genome
     public void initNetwork(){
+        //Add bias node
+        int neat_input_size = AppConfig.NEAT_INPUT_SIZE;
+        if(AppConfig.NEAT_BIAS)
+             neat_input_size ++;
         //Add input nodes
-        for(int i = 0; i < AppConfig.NEAT_INPUT_SIZE; i++){
+        for(int i = 0; i < neat_input_size; i++){
             Node n = new Node(Node.Type.INPUT);
             addNode(i,n);
         }
-
-        //Add bias node
-        {
-            Node n = new Node(Node.Type.INPUT);
-            addNode(AppConfig.NEAT_INPUT_SIZE,n);
-        }
-
         //Add output nodes
         for(int i = 0; i < AppConfig.NEAT_OUTPUT_SIZE; i++){
             Node n = new Node(Node.Type.OUTPUT);
-            addNode(AppConfig.NEAT_INPUT_SIZE+i+1,n);
+            addNode(neat_input_size+i,n);
         }
-
-        //Add hidden nodes and connections
-        for(int i = 0; i < AppConfig.NEAT_HIDDEN_SIZE; i++){
-            Node n = new Node(Node.Type.HIDDEN);
-            int node_id = AppConfig.NEAT_INPUT_SIZE+AppConfig.NEAT_OUTPUT_SIZE+i+1;
-            addNode(node_id,n);
-            //Add all input as incoming connections
-            for(int j = 0; j < AppConfig.NEAT_INPUT_SIZE; j++){
-                Node m = getNode(j);
-                Connection c = new Connection(m,n,Connection.randomWeight());
-                addConnection(c);
-                n.addIncomingConnection(c);
-                m.addOutgoingConnection(c);
+        if(AppConfig.NEAT_HIDDEN_SIZE == 0)
+        {
+            //Connect all input to all output
+            for(int i = 0; i < neat_input_size; i++){
+                for(int j = neat_input_size; j < neat_input_size+AppConfig.NEAT_OUTPUT_SIZE; j++){
+                    Node m = getNode(i);
+                    Node n = getNode(j);
+                    Connection c = new Connection(m,n,Connection.randomWeight());
+                    addConnection(c);
+                    m.addOutgoingConnection(c);
+                    n.addIncomingConnection(c);
+                }
             }
-            //Add all output as outgoing connections
-            for(int j = AppConfig.NEAT_INPUT_SIZE; j < AppConfig.NEAT_INPUT_SIZE+AppConfig.NEAT_OUTPUT_SIZE; j++){
-                Node m = getNode(j);
-                Connection c = new Connection(n,m,Connection.randomWeight());
-                addConnection(c);
-                n.addOutgoingConnection(c);
-                m.addIncomingConnection(c);
+        }else{
+            //Add hidden nodes and connections
+            for(int i = 0; i < AppConfig.NEAT_HIDDEN_SIZE; i++){
+                Node n = new Node(Node.Type.HIDDEN);
+                int node_id = neat_input_size+AppConfig.NEAT_OUTPUT_SIZE+i;
+                addNode(node_id,n);
+                //Add all input as incoming connections
+                for(int j = 0; j < neat_input_size; j++){
+                    Node m = getNode(j);
+                    Connection c = new Connection(m,n,Connection.randomWeight());
+                    addConnection(c);
+                    n.addIncomingConnection(c);
+                    m.addOutgoingConnection(c);
+                }
+                //Add all output as outgoing connections
+                for(int j = neat_input_size; j < neat_input_size+AppConfig.NEAT_OUTPUT_SIZE; j++){
+                    Node m = getNode(j);
+                    Connection c = new Connection(n,m,Connection.randomWeight());
+                    addConnection(c);
+                    n.addOutgoingConnection(c);
+                    m.addIncomingConnection(c);
+                }
             }
         }
     }
@@ -98,19 +117,25 @@ public class Genome {
     public float[] getOutputs(float[] in){
         //Reset all node values
         //Small nodes.values() is a collection of all the values in the map not the values inside a node
+        int neat_input_size = AppConfig.NEAT_INPUT_SIZE;
         for(Node n : nodes){
             //permit to check if the node is well cleaned easier
             n.setValue(Float.NaN);
         }
         //Set input nodes
-        for(int i = 0; i < AppConfig.NEAT_INPUT_SIZE; i++){
+        for(int i = 0; i < neat_input_size; i++){
             getNode(i).setValue(in[i]);
         }
-        //Set bias node
-        getNode(AppConfig.NEAT_INPUT_SIZE).setValue(1);
+
+        //Set bias node if needed
+        if(AppConfig.NEAT_BIAS)
+        {
+            getNode(AppConfig.NEAT_INPUT_SIZE).setValue(1);
+            neat_input_size++;
+        }
         //Evaluate hidden nodes
         //This code assume that the nodes are sorted by layer
-        for(int i = AppConfig.NEAT_INPUT_SIZE+AppConfig.NEAT_OUTPUT_SIZE+1; i < nodes.size(); i++){
+        for(int i = neat_input_size; i < nodes.size(); i++){
             Node n = getNode(i);
             float sum = 0;
             for(Connection c : n.incomingConnections){
@@ -122,7 +147,7 @@ public class Genome {
         }
         float out[] = new float[AppConfig.NEAT_OUTPUT_SIZE];
         //Evaluate output nodes
-        for(int i = AppConfig.NEAT_INPUT_SIZE+1; i < AppConfig.NEAT_INPUT_SIZE+AppConfig.NEAT_OUTPUT_SIZE+1; i++){
+        for(int i = neat_input_size; i < neat_input_size+AppConfig.NEAT_OUTPUT_SIZE; i++){
             Node n = getNode(i);
             float sum = 0;
             for(Connection c : n.incomingConnections){
@@ -132,13 +157,15 @@ public class Genome {
             }
             float value = sigmoid(sum);
             n.setValue(value);
-            out[i-AppConfig.NEAT_INPUT_SIZE-1] = value;
+            out[i-neat_input_size] = value;
         }
         return out;
     }
 
     public static float sigmoid(float x){
-        return (float) (1/(1+Math.exp(-x)));
+        //return (float) (1/(1+Math.exp(-x)));
+        //Change the sigmoid function from the documentation
+        return (float) (1/(1+Math.exp(-4.9*x)));
     }
 
     // Managing Mutation
@@ -155,29 +182,25 @@ public class Genome {
     }
     //Connection mutation
     public void mutAddConnection(){
-        //Select a random connection
-        //TODO: Adjust the randomness of that selection
-
-    	Connection c = new Connection(nodes.get(rand.nextInt(nodes.size())),nodes.get(rand.nextInt(nodes.size())),Connection.randomWeight());
-
-//    	connections.add(new Connection(nodes.get(rand.nextInt(nodes.size())),nodes.get(rand.nextInt(nodes.size())),Connection.randomWeight()));
-        connections.add(c);
+        //Try to create a new connection between two nodes
+        boolean creationOkay = false;
+        int i = 0;
+        //Try to create a connection 10 times if it fails just doesn't add any connection
+        while(!creationOkay && i < 10){
+            creationOkay = addConnection(nodes.get(rand.nextInt(nodes.size()-1)),nodes.get(rand.nextInt(nodes.size()-1)),Connection.randomWeight());
+            i++;
+        }
     }
-    public void mutRemoveConnection(){
-        //Select a random connection
-        connections.remove(rand.nextInt(connections.size()));
-    }
+    //Connection Mutation
     public void mutChangeConnectionWeight(){
-        //Select a random connection
-        //TODO: Adjust the random nature of weight
-        connections.get(rand.nextInt(connections.size())).setWeight(rand.nextFloat());
+        //Select a random connection and adjust the weight
+        connections.get(rand.nextInt(connections.size()-1)).setWeight(Connection.randomWeight());
     }
     //Node Mutation
     public void mutChangeConnectionState(){
-        //Select a random connection
-        connections.get(rand.nextInt(connections.size())).flipConnectionState();
+        //Select a random connection and flip the state
+        connections.get(rand.nextInt(connections.size()-1)).flipConnectionState();
     }
-
     public void mutAddNode(){
         //Select a random connection
         Connection c = connections.get(rand.nextInt(connections.size()));
@@ -190,25 +213,22 @@ public class Genome {
         Connection c1 = new Connection(c.getInputNode(),n,Connection.randomWeight());
         Connection c2 = new Connection(n,c.getOutputNode(),Connection.randomWeight());
 
-
         //Add the new node and connections
         nodes.add(n);
         connections.add(c1);
         connections.add(c2);
         synchronizeNodeIds();
     }
-    
+
     // copy the genome
     @Override
     public Genome clone() {
     	Genome copy = new Genome();
-    	
     	// nodes
     	for (Node node : nodes) {
     		Node newNode = node.clone();
     		copy.addNode(newNode);
     	}
-    	
     	// connections
     	for (Connection con : connections) {
     		Connection newCon = con.clone();
@@ -216,7 +236,6 @@ public class Genome {
     	}
     	return copy;
     }
-    
     // Print the genome in a dot file (https://en.wikipedia.org/wiki/DOT_%28graph_description_language%29)
     public void exportToDot(String filename) {
         try (FileWriter writer = new FileWriter(filename)) {
---
+++ src/test/java/TestGenome.java
@@ -0,0 +1,81 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.Assert.*;
+import com.hepl.NEAT.*;
+import com.hepl.SourceProject.App;
+
+import junit.framework.Assert;
+
+public class TestGenome {
+    @Test
+    public void testInitNetworkWithtBias() {
+        Genome g = new Genome();
+        AppConfig.NEAT_BIAS = true;
+        g.initNetwork();
+        Assert.assertEquals(AppConfig.NEAT_INPUT_SIZE + AppConfig.NEAT_OUTPUT_SIZE + AppConfig.NEAT_HIDDEN_SIZE + 1, g.nodes.size());
+    }
+    @Test
+    public void testInitNetworkWithoutBias() {
+        AppConfig.NEAT_BIAS = false;
+        Genome g = new Genome();
+        g.initNetwork();
+        Assert.assertEquals(AppConfig.NEAT_INPUT_SIZE + AppConfig.NEAT_OUTPUT_SIZE + AppConfig.NEAT_HIDDEN_SIZE, g.nodes.size());
+    }
+    @Test
+    public void testInitNetworkWithRandomWeight() {
+        AppConfig.NEAT_INIT_WEIGHT_RANDOM = true;
+        Genome g = new Genome();
+        g.initNetwork();
+        for (Connection c : g.connections) {
+            Assert.assertTrue(c.getWeight() < 1 && c.getWeight() > -1);
+        }
+    }
+    @Test
+    public void testInitNetworkWithoutHiddenLayerWithBias() {
+        AppConfig.NEAT_HIDDEN_SIZE = 0;
+        AppConfig.NEAT_BIAS = true;
+        Genome g = new Genome();
+        g.initNetwork();
+        //test number of nodes
+        Assert.assertEquals(AppConfig.NEAT_INPUT_SIZE + AppConfig.NEAT_OUTPUT_SIZE+1, g.nodes.size());
+        //test number of connections
+        Assert.assertEquals((AppConfig.NEAT_INPUT_SIZE +1 ) * AppConfig.NEAT_OUTPUT_SIZE, g.connections.size());
+    }
+    @Test
+    public void testInitNetworkWithoutHiddenLayerWithoutBias() {
+        AppConfig.NEAT_HIDDEN_SIZE = 0;
+        AppConfig.NEAT_BIAS = false;
+        Genome g = new Genome();
+        g.initNetwork();
+        //test number of nodes
+        Assert.assertEquals(AppConfig.NEAT_INPUT_SIZE + AppConfig.NEAT_OUTPUT_SIZE, g.nodes.size());
+        //test number of connections
+        Assert.assertEquals(AppConfig.NEAT_INPUT_SIZE * AppConfig.NEAT_OUTPUT_SIZE, g.connections.size());
+    }
+
+    @Test
+    public void testGetOutputs() {
+        Genome g = new Genome();
+        float [] input = new float[AppConfig.NEAT_INPUT_SIZE];
+        for (int i = 0; i < AppConfig.NEAT_INPUT_SIZE; i++) {
+            input[i] = 1;
+        }
+        g.initNetwork();
+        //Modify all weight to one to get the same output
+        for (Connection c : g.connections) {
+            c.setWeight(1);
+        }
+        float [] output = g.getOutputs(input);
+        int output_length = output.length;
+
+        int expected_output_length = AppConfig.NEAT_OUTPUT_SIZE;
+        //Print output in the test
+        for (int i = 0; i < output_length; i++) {
+            Assert.assertTrue(output[i] < 1.01 && output[i] > 0.99);
+        }
+        Assert.assertEquals(output_length, expected_output_length);
+    }
+
+}
